eaf5ae5c6f5605a44b5976fe35e89145fe54e52a
unknown <lavinotinto29@gmail.com>
2021-01-16 18:04:14 -0300

    segundo

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const navegacion = document.querySelector('.navegacion__mobile')
 const lista = document.querySelector('#lista')
 
 button.addEventListener('click',()=>{
-    button.classList.toggle('is-active')
+button.classList.toggle('is-active')
 navegacion.classList.toggle('menu_activo2')
 lista.classList.toggle('menu_activo')
 })
